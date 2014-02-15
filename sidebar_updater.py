1d62a3da74d8ea8a45dc49df7c17d6281085e085
listen2 <getifaddrs@gmail.com>
2014-02-15 15:59:13 -0500

    A year's worth of unmerged changes.

--- sidebar_updater.py
+++ sidebar_updater.py
@@ -1,23 +1,51 @@
+#!/usr/bin/python3
+
 import html.parser
 import time
-import httplib2
+import socket
+import urllib.request
 from xml.dom.minidom import parseString
 from threading import Thread
 from string import Template
+import configparser
 
-sentinel = "[~s~](/s)"
-subr_name = "diablo"
-credentials_file = "/home/listen2/login_creds"
+import lightreddit
 
-#TODO this will be unnecessary after we set $PYTHONPATH
-import sys
-if "/srv/bots/common/praw" not in sys.path:
-	sys.path.insert(0, "/srv/bots/common/praw")
-import praw
+config = configparser.ConfigParser()
+config.read("/srv/bots/sidebar_updater/sidebar_config.ini")
+
+rules = []
+for s in config.sections():
+	r = {}
+	for k in ["rname", "sentinel", "template", "user", "pass"]:
+		r[k] = config.get(s, k)
+	rules.append(r)
+
+def time_to_dhms(ti):
+	"""Takes time in unixtime; returns (days, hours, minutes, seconds)
+	"""
+	secs = int(ti - time.time())
+	days = int(secs / 86400)
+	#secs -= days * 86400
+	hours = int(secs / 3600)
+	#secs -= hours * 3600
+	mins = int(secs / 60)
+	return (days, hours, mins, secs)
+
+def time_left(ti):
+	(days, hours, mins, secs) = time_to_dhms(ti)
+	if secs < 0:
+		return "has launched!"
+	elif mins < 60:
+		return "launch in %s minutes" % (mins)
+	elif hours < 24:
+		return "launch in %s hours" % (hours)
+	else:
+		return "launch in %s days" % (days + 1)
 
 class BNetChecker(Thread):
 	"""
-	Checks whether the D3 beta realm is up, and if not, gets the message.
+	Checks whether the D3 realms are up, and if not, gets the message.
 	"""
 	def trunc(self, msg, more):
 		msg = msg.replace("\n\n", "—", 1)
@@ -27,13 +55,14 @@ class BNetChecker(Thread):
 		return msg
 
 	def run(self):
-		h = httplib2.Http(".cache")
-		resp, html = h.request("http://us.battle.net/d3/en/status", "GET")
-		_status_dom = parseString(html)
+		self.am = "AM: "
+		self.eu = "|EU: "
+		self.asia = "|AS: "
+		self.status = ""
 
-		#resp, html = h.request("http://us.launcher.battle.net/d3/en-us/patch?patchVersion=0", "GET")
-		#patch_dom = parseString(html)
-		#client_ver_string = _patch_dom.childNodes[0].childNodes[3].childNodes[3].childNodes[3].childNodes[3].childNodes[1].childNodes[3].childNodes[1].childNodes[1].childNodes[0].childNodes[0].nodeValue.rsplit(" –", 1)[0].rsplit(" ", 1)[1]
+		html = urllib.request.urlopen("http://us.battle.net/d3/en/status").read()
+
+		_status_dom = parseString(html)
 
 		regions = ["enus", "engb", "zhtw"]
 		other_regions = {"am": {"pretty":"Americas", "url":"http://us.launcher.battle.net/service/d3/alert/en-us"},
@@ -41,22 +70,34 @@ class BNetChecker(Thread):
 				"as": {"pretty":"Asia", "url":"http://sea.launcher.battle.net/service/d3/alert/en-us"}}
 		bnet_build = {}
 		client_build = {}
-		for r in regions:
-			resp, html = h.request("http://"+r+".patch.battle.net:1119/patch", "POST", "<version program='D3'><record program='Bnet' component='Win' version='1' /><record program='D3' component='enUS' version='1' /></version>")
-			_ver_dom = parseString(html)
-			bnet_build[r] = _ver_dom.childNodes[0].childNodes[1].childNodes[0].nodeValue.rstrip().rsplit(";", 1)[1]
-			client_build[r] = _ver_dom.childNodes[0].childNodes[3].childNodes[0].nodeValue.rstrip().rsplit(";", 1)[1]
 
-		b = _status_dom.childNodes[0].childNodes[3].childNodes[0].childNodes[3].childNodes[1].childNodes[1].childNodes[3].childNodes[1].childNodes[1]
+		global _db_dir_inner
+		_db_dir_inner = None
+		def find_html_class(n, cl):
+			global _db_dir_inner
+			try:
+				if n._attrs["class"].value == "db-directory-inner":
+					_db_dir_inner = n
+					return
+			except KeyError:
+				pass
+			except AttributeError:
+				pass
+			except TypeError:
+				pass
+
+			for e in n.childNodes:
+				find_html_class(e, cl)
+		find_html_class(_status_dom, "a")
+
 		status_response = {}
-		status_response["am"] = b.childNodes[1].childNodes[1].childNodes[3].childNodes[1].childNodes[1]._attrs["class"].nodeValue.split()[1]
-		status_response["eu"] = b.childNodes[3].childNodes[1].childNodes[3].childNodes[1].childNodes[1]._attrs["class"].nodeValue.split()[1]
-		status_response["as"] = b.childNodes[5].childNodes[1].childNodes[3].childNodes[1].childNodes[1]._attrs["class"].nodeValue.split()[1]
-		self.am = "AM: " + ("[online](/bnetOnline '" + bnet_build["enus"] + ", " + client_build["enus"] + "')") if status_response["am"] == "up" else "[Offline](/bnetOffline)"
-		self.eu = "|EU: " + (("[online](/bnetOnline '" + bnet_build["engb"] + ", " + client_build["engb"] + "')") if status_response["eu"] == "up" else "[Offline](/bnetOffline)")
-		self.asia = "|AS: " + (("[online](/bnetOnline '" + bnet_build["zhtw"] + ", " + client_build["zhtw"] + "')") if status_response["as"] == "up" else "[Offline](/bnetOffline)")
+		status_response["am"] = _db_dir_inner.childNodes[1].childNodes[1].childNodes[3].childNodes[1].childNodes[1]._attrs["class"].nodeValue.split()[1]
+		status_response["eu"] = _db_dir_inner.childNodes[3].childNodes[1].childNodes[3].childNodes[1].childNodes[1]._attrs["class"].nodeValue.split()[1]
+		status_response["as"] = _db_dir_inner.childNodes[5].childNodes[1].childNodes[3].childNodes[1].childNodes[1]._attrs["class"].nodeValue.split()[1]
+		self.am = "AM: " + (("[online](/bnetOnline)") if status_response["am"] == "up" else "[Offline](/bnetOffline)")
+		self.eu = "|EU: " + (("[online](/bnetOnline)") if status_response["eu"] == "up" else "[Offline](/bnetOffline)")
+		self.asia = "|AS: " + (("[online](/bnetOnline)") if status_response["as"] == "up" else "[Offline](/bnetOffline)")
 
-		self.status = ""
 		for r in ["am", "eu", "as"]:
 			if not status_response[r] == "up":
 				resp, alertMessage = h.request(other_regions[r]["url"], "GET")
@@ -65,115 +106,143 @@ class BNetChecker(Thread):
 					self.status += "%s alert\n------------------\n\n%s\n\n" % (other_regions[r]["pretty"], alertMessage)
 				return
 
-'''
-class SlashdiabloChecker(Thread):
+class IRCChecker(Thread):
 	"""
-	Gets the number of users and games from slashdiablo
-	"""
-	def run(self):
-		self.info = "[Slashdiablo](/r/slashdiablo) (server info unavailable)\n\n---\n\n"
-		h = httplib2.Http(".cache")
-		resp, html = h.request("http://ewalk0871.no-ip.org:8080/server.dat", "GET", headers={"Range": "bytes=0-150"})
-
-		d = html.decode("utf8").split("\n")
-		self.info = "[Slashdiablo](/r/slashdiablo): %s players in %s games\n\n---\n\n" % (d[5][9:], d[4][6:])
-'''
-
-class MumbleChecker(Thread):
-	"""
-	Gets the number of users currently in mumble
+	Gets the number of users currently in IRC
 	"""
 	def run(self):
-		h = httplib2.Http(".cache")
-		resp, j = h.request("http://clanforge.multiplay.co.uk/public/servers.pl?event=Online;opt=ServerXml;serverid=140233", "GET")
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		except socket.OSError as e:
+			print("Failed to create socket: %s" % (e))
 
-		_mumble_dom = parseString(j)
-		self.num_users = _mumble_dom.getElementsByTagName("numplayers")[0].firstChild.data
-		self.max_users = _mumble_dom.getElementsByTagName("maxplayers")[0].firstChild.data
+		s.settimeout(5)
 
-#create threads to handle slow (network-dependent) fetches
-threads = {}
-threads["bnet"] = BNetChecker()
-#threads["slashdiablo"] = SlashdiabloChecker()
-threads["mumble"] = MumbleChecker()
+		try:
+			remote_ip = socket.gethostbyname("tucana.whatbox.ca")
+		except socket.gaierror:
+			print("Hostname could not be resolved.")
 
-for t in threads:
-	threads[t].start()
+		try:
+			s.connect((remote_ip, 42666))
+		except socket.timeout:
+			print("Socket connection timed out.")
+			self.irc_size = ""
+			s.close()
+			return
 
-# Create reddit object and login
-r = praw.Reddit(user_agent="/r/%s sidebar updater; PRAW. Contact /u/listen2]" % (subr_name))
-with open(credentials_file, "r") as f:
-	user = f.read().rstrip()
-	passwd = f.read().rstrip()
-r.login(user, passwd)
+		try:
+			s.sendall(b"irc_users\n")
+		except socket.error:
+			print("Send failed")
 
-subreddit = r.get_subreddit(subr_name)
+		try:
+			self.irc_size = " (%s users)" % (s.recv(4096).decode("utf8"))
+		except socket.timeout:
+			self.irc_size = ""
 
-# Grab current settings
-subr_info = subreddit.get_settings()
-subr_desc = subr_info["description"][(subr_info["description"].find(sentinel)+len(sentinel)):]
+		s.close()
 
-# There has to be a way to get rid of the HTMLParser ;__;
-HP = html.parser.HTMLParser()
-subr_desc = HP.unescape(subr_desc)
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		except socket.OSError as e:
+			print("Failed to create socket: %s" % (e))
 
-with open("/tmp/irc_diablo_size", "r") as f:
-	irc_size = f.read()
+		s.settimeout(5)
 
-lastUpdated = "[Last updated at " + time.strftime("%H:%M:%S UTC", time.gmtime()) + "](/smallText)"
+		try:
+			remote_ip = socket.gethostbyname("tucana.whatbox.ca")
+		except socket.gaierror:
+			print("Hostname could not be resolved.")
 
-# Wait for threads to finish, if they haven't already
-for t in threads:
-	threads[t].join()
+		try:
+			s.connect((remote_ip, 42666))
+		except socket.timeout:
+			print("Socket connection timed out.")
+			self.irc_size = ""
+			s.close()
+			return
 
-# Update subreddit description
-newDescription = Template("""Diablo III game server status
+		try:
+			s.sendall(b"mumble_users\n")
+		except socket.error:
+			print("Send failed")
 
-${am}${eu}${asia}
-:--|:--|:--
+		try:
+			self.mumble_size = " (%s users)" % (s.recv(4096).decode("utf8").split("\n", maxsplit=1)[0])
+		except socket.timeout:
+			self.mumble_size = ""
 
-$alert
+		s.close()
 
----
+for g in rules:
+	#create threads to handle slow (network-dependent) fetches
+	threads = {}
+	threads["bnet"] = BNetChecker()
+	threads["irc"] = IRCChecker()
 
+	for t in threads:
+		threads[t].start()
 
-$lastUpdated
+	# Create lightreddit object
+	r = lightreddit.RedditSession(g["user"], g["pass"], "/r/diablo sidebar updater. Contact /u/listen2")
 
-----
+	# Grab current settings
+	subr_info = r.get_subreddit_settings(g["rname"])
+	subr_desc = subr_info["description"][(subr_info["description"].find(g["sentinel"])+len(g["sentinel"])):]
 
+	# There has to be a way to get rid of the HTMLParser
+	HP = html.parser.HTMLParser()
+	subr_desc = HP.unescape(subr_desc)
 
-[Heaven](http://dd.reddit.com/r/Diablo)[Select a theme](/#themeselector)[Hell](http://www.reddit.com/r/Diablo)
+	# Calculate time until release.
+	#secs = int(1337065200 - time.time())+3600 # 15 May 2012 00:00:00 PDT # Add 3600 because it truncates; this simulates a round up.
+	end = 1395705600
+	total_seconds = 9158400
+	releaseDateCounter = "[Reaper of Souls %s](/releaseCountdown)\n\n" % time_left(end)
+	percentage = int(((time.time() - 1387411200) / (total_seconds)) * 10)*10
+	releaseDateCounter += "[%s%%](/%sp)[%s%%](/%sn)\n\n" % (percentage, percentage, 100-percentage, 100-percentage)
 
-----
+	#releaseDateCounter += "[Diablo 3 Europe " + time_left("eu") + "](/releaseCountdown)\n\n"
+	#releaseDateCounter += "[Diablo 3 Asia " + time_left("sea") + "](/releaseCountdown)\n\n"
 
-[@redditdiablo (Twitter)](http://www.twitter.com/redditdiablo) [/r/Diablo YouTube Channel](http://www.youtube.com/redditdiablo) [/r/Diablo@twitch.tv](http://www.justin.tv/rdiablo) [Steam Group](http://steamcommunity.com/groups/rdiablo)
+	with open("/tmp/rdiablo_thread_gear_tid", "r") as f:
+		tid_gear = f.read().rstrip()
 
+	with open("/tmp/rdiablo_thread_loot_tid", "r") as f:
+		tid_loot = f.read().rstrip()
 
-**Community Links**
+	with open("/tmp/rdiablo_thread_questions_tid", "r") as f:
+		tid_questions = f.read().rstrip()
 
-* [/r/Diablo browser extension](/10f7ce)
+	with open("/tmp/rdiablo_thread_challenge_tid", "r") as f:
+		tid_challenge = f.read().rstrip()
 
-* [IRC channel](http://chat.mibbit.com/?url=irc://irc.esper.net/diablo) ($irc_size users)
+	lastUpdated = "[Last updated at " + time.strftime("%H:%M:%S UTC", time.gmtime()) + "](/smallText)"
 
-    irc.esper.net #diablo
+	# Wait for threads to finish, if they haven't already
+	for t in threads:
+		threads[t].join()
 
-* [Mumble](http://tinyurl.com/7ulkl6l) ($mumble_users/$mumble_max users)
+	# Update subreddit description
+	with open(g["template"], "r") as t:
+		t = t.read().rstrip()
+	newDescription = Template(t)
 
-* [Slashdiablo](/r/slashdiablo)
-
-* [How to get Battletag flair](/qyfqt)
-
-$sentinel$subr_desc""")
-newDescription = newDescription.substitute(
-	am=threads["bnet"].am,
-	eu=threads["bnet"].eu,
-	asia=threads["bnet"].asia,
-	alert=threads["bnet"].status,
-	mumble_users=threads["mumble"].num_users,
-	mumble_max=threads["mumble"].max_users,
-	irc_size=irc_size,
-	lastUpdated=lastUpdated,
-	sentinel=str(sentinel),
-	subr_desc=subr_desc)
-subreddit.update_settings(description=newDescription)
-#print(newDescription)
+	newDescription = newDescription.substitute(
+		release=releaseDateCounter,
+		am=threads["bnet"].am,
+		eu=threads["bnet"].eu,
+		asia=threads["bnet"].asia,
+		alert=threads["bnet"].status,
+		irc_size=threads["irc"].irc_size,
+		mumble_size=threads["irc"].mumble_size,
+		lastUpdated=lastUpdated,
+		gear=tid_gear,
+		loot=tid_loot,
+		questions=tid_questions,
+		challenge=tid_challenge,
+		sentinel=str(g["sentinel"]),
+		subr_desc=subr_desc)
+	r.wiki_write(g["rname"], "config/sidebar", newDescription)
+	#print(newDescription)
